73188242d607573c87185bc817b7f50341940917
joao <joao_trombeta@hotmail.com>
2017-09-19 19:29:03 -0300

    change minor bugs

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
-$( function() {
-  $( "#sortable1, #sortable2, #sortable3, #sortable4" ).sortable({
-    items: "li:not(.ui-state-disabled)"
-    
-  });
-  $( "#sortable1, #sortable2, #sortable3, #sortable4" ).sortable({
-    connectWith: ".connectedSortable"
-  });
-
- 
-  $( "#sortable1 li, #sortable2 li, #sortable3 li, #sortable4 li" ).disableSelection();
-
-  $("#btn1").click(function(){
+function newli() {
     var afazer = $("#todo").val();
     $("#todo").val('');
-    $("label.active").attr('class', '');
     var atual =  $("#sortable1").html();
     if (afazer != null && afazer != "")
     $("#sortable1").html(atual + "<li class=\"collection-item\">" + afazer + "</li>");
+}
+
+$(function() {
+  $("#sortable1, #sortable2, #sortable3, #sortable4").sortable( {
+    items: "li:not(.ui-state-disabled)"
+  });
+  $("#sortable1, #sortable2, #sortable3, #sortable4").sortable( {
+    connectWith: ".connectedSortable"
   });
 
-  $("#btn2").click(function(){
+  $("#sortable1 li, #sortable2 li, #sortable3 li, #sortable4 li").disableSelection();
+
+  $("#btn1").click(function() {
+    newli();
+  });
+
+  $("input#todo").keypress(function(e) {
+    if(e.which == 13) {
+      newli();
+    }
+  });
+
+  $("#btn2").click(function() {
     $("#sortable4 li").not(".ui-state-disabled").remove();
   });
 
-} );
+});
